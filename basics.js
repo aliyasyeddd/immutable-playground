f543d976c3767a7bf3ff9aea0d1d1ada9a5206e5
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-10 14:30:17 +0530

    Convert Immutable back to plain JS

--- basics.js
+++ basics.js
@@ -1,4 +1,4 @@
-import { List } from "immutable"
+import { List, Map } from "immutable"
 
 const names = ["ramsha", "nisha", "alina" ];
 
@@ -9,3 +9,23 @@ console.log(typeof immutableNames);
 console.log(immutableNames.toString());
 console.log('Is regular array?', Array.isArray(immutableNames)); // false
 console.log('Is Immutable List?', immutableNames instanceof List); // true
+
+
+let userBio = {
+    name: "Aliya",
+    id: 224,
+    email: "mahaboobaliya02@gmail.com"
+}
+
+const immutableUser = Map(userBio)
+
+//.toJS() → Converts Immutable data → Readable, plain JavaScript.
+const plainUser = immutableUser.toJS();
+const plainNames = immutableNames.toJS();
+
+console.log('Immutable Map:', immutableUser.toString());
+console.log('Plain JS Object:', plainUser);
+
+console.log('Immutable List:', immutableNames);
+console.log('Plain JS Array:', plainNames);
+
